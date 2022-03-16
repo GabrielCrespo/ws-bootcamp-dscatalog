876213feecc46425c4a607008599e069458caa17
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-03-15 22:46:05 -0300

    DTO

--- backend/src/main/java/com/gabriel/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/gabriel/dscatalog/resources/CategoryResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gabriel.dscatalog.entities.Category;
+import com.gabriel.dscatalog.dto.CategoryDTO;
 import com.gabriel.dscatalog.services.CategoryService;
 
 @RestController
@@ -21,8 +21,8 @@ public class CategoryResource {
 
 	@GetMapping
 	@Transactional(readOnly = true)
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/gabriel/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gabriel/dscatalog/services/CategoryService.java
@@ -1,11 +1,12 @@
 package com.gabriel.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.gabriel.dscatalog.entities.Category;
+import com.gabriel.dscatalog.dto.CategoryDTO;
 import com.gabriel.dscatalog.repositories.CategoryRepository;
 
 @Service
@@ -14,8 +15,11 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		return repository.findAll()
+				.stream()
+				.map(category -> new CategoryDTO(category))
+				.collect(Collectors.toList());
 	}
 	
 }
